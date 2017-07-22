c5964089f04b7c43663cdeb1eb1b17295282ab2d
Imran Askem <imran.askem@gmail.com>
2017-07-22 15:58:32 +0100

    Added physics to comet

--- src/systems/StarSpawner.js
+++ src/systems/StarSpawner.js
@@ -4,4 +4,16 @@ export default class extends Phaser.GameObjectFactory {
     constructor (game) {
         super(game)
     }
+
+    CreateStars() {
+        let xspawn = this.SetXSpawn()
+
+                
+    }
+
+    SetXSpawn() {
+        let xspawn = Math.floor(Math.random() * this.world.bounds.width)
+
+        return xspawn
+    }
 }
