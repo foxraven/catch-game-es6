72ccbbb5830e6367cd44e215f960b392c935250c
Imran Askem <imran.askem@gmail.com>
2017-07-22 15:20:18 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  gameWidth: 450,
+  gameHeight: 800,
+  localStorageName: 'phaseres6webpack'
+}
---
+++ src/states/Game.js
@@ -0,0 +1,67 @@
+/* globals __DEV__ */
+import Phaser from 'phaser'
+import Mushroom from '../sprites/Mushroom'
+import Bucket from '../bucket/Bucket'
+import Constants from '../constants'
+import StarSpawner from '../systems/StarSpawner'
+import Comet from '../stars/Comet'
+
+export default class extends Phaser.State {
+  init () {}
+  preload () {}
+
+  create () {
+    let UIfont = 'Bangers'
+    let banner = this.add.text(this.world.centerX, this.game.height - 30, Constants.bannerText)
+
+    banner.font = UIfont
+    banner.padding.set(10, 16)
+    banner.fontSize = 40
+    banner.fill = '#77BFA3'
+    banner.smoothed = false
+    banner.anchor.setTo(0.5)
+
+    let score = this.add.text(this.game.width - 200, 10, Constants.score)
+    score.font = UIfont
+    score.padding.set(10, 16)
+    score.fill = '#FFFFFF'
+    let fuel = this.add.text(10, 10, Constants.fuel)
+    fuel.font = UIfont
+    fuel.padding.set(10, 16)
+    fuel.fill = '#FFFFFF'
+
+    this.mushroom = new Mushroom({
+      game: this.game,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: 'mushroom'
+    })
+
+    this.game.add.existing(this.mushroom)
+
+    this.bucket = new Bucket({
+      game: this.game,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: 'mushroom'
+    })
+
+    this.game.add.existing(this.bucket)  
+    
+    this.comet = new Comet({
+      game: this.game,
+      x: this.world.centerX,
+      y: this.world.top,
+      asset: 'comet'
+    })
+
+    this.game.add.existing(this.comet)
+  }
+
+  render () {
+    // if (__DEV__) {
+    //   this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    //   this.game.debug.spriteInfo(this.bucket, 32, 32)
+    // }
+  }
+}
